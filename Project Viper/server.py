1d937aad9c7bb0b3c61e9a63b6e0eaada1e4a1a6
MountainLover44 <110260666+MountainLover44@users.noreply.github.com>
2023-02-02 23:37:04 -0500

    Delete Project Viper directory

--- Project Viper/server.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app 
-#this is where we import controllers when we make it
-from flask_app.controller import login_controller
-#ALL CONTROLLERS ARE IMPORTED HERE
-
-if __name__=="__main__":
-    app.run(debug=True,port=5001)
